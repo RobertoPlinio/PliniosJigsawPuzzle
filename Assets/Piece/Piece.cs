84eb6db7907a6945c53ccee3df10336eef5f681f
RobertoPlinio <robertoppjr@gmail.com>
2022-02-08 22:42:48 -0300

    Namespace JigsawPuzzle inserted to all relevant scripts

--- Assets/Piece/Piece.cs
+++ Assets/Piece/Piece.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piece : MonoBehaviour
+namespace JigsawPuzzle
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Piece : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
