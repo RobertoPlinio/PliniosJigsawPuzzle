3fa6973506975a01005a13beb8c8eeaae3dd210d
RobertoPlinio <robertoppjr@gmail.com>
2022-03-01 19:54:23 -0300

    Updated Piece to test using PieceInfo to get the desired mesh

--- Assets/Piece/Piece.cs
+++ Assets/Piece/Piece.cs
@@ -6,6 +6,32 @@ namespace JigsawPuzzle
 {
     public class Piece : MonoBehaviour
     {
+        public enum Side
+        {
+            Edge,
+            Tab,
+            Hole
+        }
+
         public bool IsCorner;
+        public PieceInfo info;
+
+        [ContextMenu("Debug Init")]
+        void DebugInit() => Init(info);
+
+        public void Init(PieceInfo _info) {
+            info = _info;
+
+            IsCorner = info.right == Side.Edge || info.left == Side.Edge
+                || info.bottom == Side.Edge || info.up == Side.Edge;
+
+            GetComponent<MeshFilter>().mesh = info.equivalentMesh;
+
+            MeshRenderer mr = GetComponent<MeshRenderer>();
+            if(mr.materials.Length < 2) {
+                Material[] mats = new Material[] { mr.material, mr.material };
+                mr.materials = mats;
+            }
+        }
     }
 }
